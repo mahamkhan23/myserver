cecfd2659ac900c8f010b099007e9d6049a93a84
unknown <toobasaleem994@gmail.com>
2023-05-05 23:09:57 +0500

    firstCommit

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ app.get('/', (req, res) => {
 
 
 app.get('/about', (req, res) => {
-    res.send('hello from maham mishal maria')
+    res.send('hello from maham')
 })
+
+app.get('/contact', (req, res) => {
+    res.send('<h1>hello from contact page</h1>')
+})
+app.get('/homepage', (req, res) => {
+    res.send('<h1>hello from home page</h1>')
+})
+
 
 app.listen(port, () => {
     console.log('Example app listening on port ${port}')
